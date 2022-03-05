c7149390e5c0e77f5e3e4a410c2f8f4a43c9b72f
isaquinley <quinteroisabella@gmail.com>
2022-03-05 10:00:49 -0500

    Adding validation utils

--- utils/formats.js
+++ utils/formats.js
@@ -1,33 +1,45 @@
 function formatDate(serial) {
-  let utc_days = Math.floor(serial - 25569);
-  //console.log(Math.floor(serial - 25569));
-  let utc_value = utc_days * 86400;
-  let date_info = new Date(utc_value * 1000);
-  return date_info.toISOString().slice(0, 10);
+  if (serial) {
+    let utc_days = Math.floor(serial - 25569);
+    //console.log(Math.floor(serial - 25569));
+    let utc_value = utc_days * 86400;
+    let date_info = new Date(utc_value * 1000);
+    return date_info.toISOString().slice(0, 10);
+  } else {
+    return "2000-00-00";
+  }
 }
 
 function formatRun_Time(serial) {
-  let hour = Math.floor(serial * 24);
-  let minute = Math.floor(Math.abs(serial * 24 * 60) % 60);
-  let second = Math.floor(Math.abs(serial * 24 * 60 * 60) % 60);
-  hour > 12 ? (hour = hour - 12) : (hour = hour);
-  hour = hour < 10 ? "0" + hour : hour;
-  minute = minute < 10 ? "0" + minute : minute;
-  second = second < 10 ? "0" + second : second;
-  //console.log(`${hour}:${minute}:${second}`);
-  return `${hour}:${minute}:${second}`;
+  if (serial) {
+    let hour = Math.floor(serial * 24);
+    let minute = Math.floor(Math.abs(serial * 24 * 60) % 60);
+    let second = Math.floor(Math.abs(serial * 24 * 60 * 60) % 60);
+    hour > 12 ? (hour = hour - 12) : (hour = hour);
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+    //console.log(`${hour}:${minute}:${second}`);
+    return `${hour}:${minute}:${second}`;
+  } else {
+    return "00:00:00";
+  }
 }
 
 function formatRun_Time_hour(serial) {
-  let hour = Math.floor(serial * 24);
-  let minute = Math.floor(Math.abs(serial * 24 * 60) % 60);
-  let second = Math.floor(Math.abs(serial * 24 * 60 * 60) % 60);
-  hour > 12 ? (hour = hour - 12) : (hour = hour);
-  hour = hour < 10 ? "0" + hour : hour;
-  minute = minute < 10 ? "0" + minute : minute;
-  second = second < 10 ? "0" + second : second;
-  //console.log(`${hour}:${minute}`);
-  return `${hour}:${minute}`;
+  if (condition) {
+    let hour = Math.floor(serial * 24);
+    let minute = Math.floor(Math.abs(serial * 24 * 60) % 60);
+    let second = Math.floor(Math.abs(serial * 24 * 60 * 60) % 60);
+    hour > 12 ? (hour = hour - 12) : (hour = hour);
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+    //console.log(`${hour}:${minute}`);
+    return `${hour}:${minute}`;
+  } else {
+    return "00:00";
+  }
 }
 
 module.exports = { formatDate, formatRun_Time, formatRun_Time_hour };
